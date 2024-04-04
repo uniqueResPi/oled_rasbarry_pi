c9e32542abcc66d2cb51c17e3809c60912242c7d
mateen <mateen159357@gmail.com>
2024-04-04 14:27:55 +0500

    new6 v5

--- pi_v5.py
+++ pi_v5.py
@@ -162,8 +162,10 @@ def parse_response(response_data, command):
         print(totalColumns)
         for i in range(min(totalRows, MAX_ROWS)):
             inner_array = all_cell_values[i]
+            print(all_cell_values[i])
             for j in range(min(totalColumns, MAX_COLUMNS)):
                 global allCellValues
+                print(inner_array[j])
                 allCellValues[i][j] = inner_array[j]
 #         print("now all 2d table is parse and print value:")
 #         for row in allCellValues:
